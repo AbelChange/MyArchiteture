539e6a8c00f43797014fd53843052b85f49d5895
HaoShuaihui <765472289@qq.com>
2023-11-02 23:15:53 +0800

    flow WhileSubscribed 用法

--- app/src/main/java/com/ablec/myarchitecture/logic/transformation/TransformationViewModel.kt
+++ app/src/main/java/com/ablec/myarchitecture/logic/transformation/TransformationViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.*
 import com.ablec.myarchitecture.data.Person
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
+import kotlin.random.Random
 
 /**
  * @Description:
@@ -53,16 +54,19 @@ class TransformationViewModel(app: Application, savedStateHandle: SavedStateHand
         return rawLiveData.distinctUntilChanged()
     }
 
-    /**
-     * 下游停止收集时取消流,2000是为了防止配置修改引起的意外取消
-     * 如果downStream  downStream2 都不再收集 才 取消该流
-     * 可避免重复创建上游带来的资源浪费
-     */
+
     private val _whileSubscribed: Flow<Int> = flow<Int> {
-        emit(1)
-        emit(2)
-        delay(3000)
-        emit(3)
+        //init gps
+        while (true) {
+            //模拟gps.getLocation()
+            emit(Random(3).nextInt())
+            delay(2000)
+        }
+        //Sharing is started when the first subscriber appears,
+        // stops when the last subscriber disappears after timeoutsMillis(by default 0)
+        // keeping the replay cache forever (by default).
+        //1.该策略可延迟热流开始时间 （1stFragment 不需要渲染位置信息, 2ndFragment 需要渲染位置信息）
+        //2.2000ms可避免因AC配置变更导致的意外stop,从而再次订阅时 flow重新开始
     }.shareIn(viewModelScope, SharingStarted.WhileSubscribed(2000))
         .distinctUntilChanged { old, new ->
             old - new == 0
